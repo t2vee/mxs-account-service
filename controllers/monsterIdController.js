93033a3f327829917f7ecf99195f41360f6ca191
t2vee <68762600+t2vee@users.noreply.github.com>
2024-05-13 19:40:53 +1000

    Fixed MonsterId because for whatever reason it was completely different to ever other generator

--- controllers/monsterIdController.js
+++ controllers/monsterIdController.js
@@ -1,25 +1,12 @@
 const monster = require('../utils/monsterid.min.js');
-const DEFAULT_SIZE = 128;
 /**
  * Generates and sends a MonsterId image based on the provided hash.
  * @param {Object} req - The request object.
  * @param {Object} res - The response object.
  */
 exports.generateMonsterIDImage = (req, res) => {
-    let width = DEFAULT_SIZE;
-    let height = DEFAULT_SIZE;
-
-    if (req.query.size) {
-        width = req.query.size;
-        height = req.query.size;
-    }
-    if (req.query.width) {
-        width = req.query.width;
-    }
-    if (req.query.height) {
-        height = req.query.height;
-    }
-    const img = monster.getAvatar(req.params.name, width, height);
+    const { hash } = req.params;
+    const img = monster.getAvatar(hash, 128, 128);
     res.setHeader('Content-Type', 'image/png');
     res.send(img);
 };
