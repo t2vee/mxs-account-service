61a96851ff2d0f272c0ec3f396cc003fa17e5681
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-06 01:35:38 +1000

    app: removed process image function/route since its now done within the worker. this app has become a simple nodejs wrapper for nsfwjs

--- controllers/checkController.js
+++ controllers/checkController.js
@@ -1,7 +1,5 @@
 const { convert } = require('../lib/imageConverter');
 const { getModel } = require('../lib/modelLoader');
-const sharp = require('sharp');
-const FormData = require('form-data');
 
 async function classifyImage(req, res) {
     const model = getModel();
@@ -22,29 +20,4 @@ async function classifyImage(req, res) {
     }
 }
 
-async function processImage(req, res) {
-    if (!req.file) {
-        return res.status(400).send("Missing image multipart/form-data");
-    }
-    try {
-        const image = sharp(req.file.buffer);
-        const metadata = await image.metadata();
-        const minDim = Math.min(metadata.width, metadata.height);
-        const left = (metadata.width - minDim) / 2;
-        const top = (metadata.height - minDim) / 2;
-        const processedImage = await image
-            .extract({ left: Math.floor(left), top: Math.floor(top), width: minDim, height: minDim })
-            .resize(300, 300, { fit: 'inside' })
-            .webp({ quality: 90 })
-            .toBuffer();
-        const formData = new FormData();
-        formData.append('image', processedImage, { filename: 'image.webp', contentType: 'image/webp' });
-        res.set('Content-Type', `multipart/form-data; boundary=${formData.getBoundary()}`);
-        formData.pipe(res);
-    } catch (error) {
-        console.error('Error processing image:', error);
-        res.status(406).send('Failed to process image');
-    }
-}
-
-module.exports = { classifyImage, processImage };
+module.exports = { classifyImage };
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,5 @@ const upload = multer();
 const router = express.Router();
 
 router.post('/avatar-service/v1/avatar/check-nsfw', upload.single("file"), checkController.classifyImage)
-router.post('/avatar-service/v1/avatar/preprocess', upload.single("file"), checkController.processImage)
 
 module.exports = router;
